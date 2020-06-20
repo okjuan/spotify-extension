ed22fb3bf9dcf61dfcdd88b74dc1e646d4a8cda3
davidnguyen179 <dzungnguyen179@gmail.com>
2020-06-20 21:18:16 +0900

    Update unit test

--- src/test/parse.spec.ts
+++ src/test/parse.spec.ts
@@ -15,12 +15,16 @@ describe('testing parse', () => {
 
     expect(result).toEqual({
       title: 'Jingle Bells',
-      artist: 'Boney M.',
+      artist: {
+        name: 'Boney M.',
+        url: 'https://open.spotify.com/artist/54R6Y0I7jGUCveDTtI21nb',
+      },
       isPlaying: true,
       coverPhoto: 'https://i.scdn.co/image/ab67616d00001e0230da6905883b7cc2441d7abe',
       uri: 'spotify:track:4JRSFd7IbVbEN3BAJVJFJf',
       progressMs: 9488,
       durationMs: 208440,
+      trackUrl: 'https://open.spotify.com/track/4JRSFd7IbVbEN3BAJVJFJf',
       context: {
         type: 'playlist',
         href: 'https://api.spotify.com/v1/playlists/5d5HXxz6RG9fzqXbhwbqJT',
@@ -34,17 +38,21 @@ describe('testing parse', () => {
     const result = parse({ ...rawData, context: null });
     expect(result).toEqual({
       title: 'Jingle Bells',
-      artist: 'Boney M.',
+      artist: {
+        name: 'Boney M.',
+        url: 'https://open.spotify.com/artist/54R6Y0I7jGUCveDTtI21nb',
+      },
       isPlaying: true,
       coverPhoto: 'https://i.scdn.co/image/ab67616d00001e0230da6905883b7cc2441d7abe',
       uri: 'spotify:track:4JRSFd7IbVbEN3BAJVJFJf',
       progressMs: 9488,
       durationMs: 208440,
       context: undefined,
+      trackUrl: 'https://open.spotify.com/track/4JRSFd7IbVbEN3BAJVJFJf',
     });
   });
 
-  it('should return data without "artists" & "images', () => {
+  it('should return data without "artists" & "images"', () => {
     const result = parse({
       ...rawData,
       item: {
@@ -59,12 +67,16 @@ describe('testing parse', () => {
 
     expect(result).toEqual({
       title: 'Jingle Bells',
-      artist: '',
+      artist: {
+        name: '',
+        url: '',
+      },
       isPlaying: true,
       coverPhoto: '',
       uri: 'spotify:track:4JRSFd7IbVbEN3BAJVJFJf',
       progressMs: 9488,
       durationMs: 208440,
+      trackUrl: 'https://open.spotify.com/track/4JRSFd7IbVbEN3BAJVJFJf',
       context: {
         type: 'playlist',
         href: 'https://api.spotify.com/v1/playlists/5d5HXxz6RG9fzqXbhwbqJT',
